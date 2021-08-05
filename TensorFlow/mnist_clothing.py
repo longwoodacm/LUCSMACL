fe4a650893e5f9cdc219738b0b026435af616501
daniel millson <danielmillson923@gmail.com>
2021-08-04 21:03:30 -0400

    Pushing old change

--- TensorFlow/mnist_clothing.py
+++ TensorFlow/mnist_clothing.py
@@ -47,7 +47,7 @@ model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)), # Input layer, 28x28=784 nodes
     keras.layers.Dense(128, activation='relu'), # Hidden layer, 128 nodes
     keras.layers.Dropout(0,2),
-    keras.layers.Dense(128, activation='sigmoid')
+    keras.layers.Dense(128, activation='sigmoid'),
     keras.layers.Dense(10, activation='softmax') # Output layer, 10 nodes
 ])
 
